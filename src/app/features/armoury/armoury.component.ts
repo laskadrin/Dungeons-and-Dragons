9386c7688840481f6263b0362ca3e2658d8a9407
Vitalii <laskadrin@gmail.com>
2022-06-02 18:28:53 +0300

    fixes

--- src/app/features/armoury/armoury.component.ts
+++ src/app/features/armoury/armoury.component.ts
@@ -3,12 +3,14 @@ import { Component, OnInit } from '@angular/core';
 export interface WeaponElement {
   name: string;
   damage: string;
+  damageType: string;
   specifics: string;
   properties: string;
   characteristic: string;
 }
 export interface ArmourElement {
   name: string;
+  type: string;
   class: string;
   specifics: string;
   providing: string;
@@ -17,47 +19,47 @@ export interface ArmourElement {
 
 
 const WEAPON_DATA: WeaponElement[] = [
-  { name: 'Кинджал', damage: '1d4 колюча', specifics: 'Бонус +3 до атаки зі спини, можна метати(без бонусу)', properties: 'Легка зброя', characteristic: 'Спритність' },
-  { name: 'Меч', damage: '1d6 рубляча/колюча', specifics: '', properties: 'Легка зброя', characteristic: 'Сила/Спритність' },
-  { name: 'Молот(Булава)', damage: '1d6 дробляча/колюча', specifics: '', properties: 'Легка зброя', characteristic: 'Сила' },
-  { name: 'Сокира', damage: '1d6 рубляча', specifics: '', properties: 'Легка зброя', characteristic: 'Сила/Спритність' },
-  { name: 'Спис', damage: '1d6 колюча', specifics: 'Можна метати', properties: 'Легка зброя', characteristic: 'Спритність' },
-  { name: 'Посох', damage: '1d4 дробляча', specifics: 'Бонус до магічної атаки +1', properties: 'Важка зброя', characteristic: 'Сила/Спритність' }
+  { name: 'Кинджал', damage: '1d4', damageType: 'Колюча', specifics: 'Бонус +3 до атаки зі спини, можна метати(без бонусу)', properties: 'Легка зброя', characteristic: 'Спритність' },
+  { name: 'Меч', damage: '1d6', damageType: 'Рубляча/колюча', specifics: '', properties: 'Легка зброя', characteristic: 'Сила/Спритність' },
+  { name: 'Молот(Булава)', damage: '1d6', damageType: 'Дробляча/колюча', specifics: '', properties: 'Легка зброя', characteristic: 'Сила' },
+  { name: 'Сокира', damage: '1d6', damageType: 'Рубляча', specifics: '', properties: 'Легка зброя', characteristic: 'Сила/Спритність' },
+  { name: 'Спис', damage: '1d6', damageType: 'Колюча', specifics: 'Можна метати', properties: 'Легка зброя', characteristic: 'Спритність' },
+  { name: 'Посох', damage: '1d4', damageType: 'Дробляча', specifics: 'Бонус до магічної атаки +1', properties: 'Важка зброя', characteristic: 'Сила/Спритність' }
 ];
 const RANGE_WEAPON_DATA: WeaponElement[] = [
-  { name: 'Легкий арбалет', damage: '1d8 Дробляча/Колюча', specifics: 'Перезарядка - основна дія', properties: 'Важка зброя', characteristic: 'Спритність' },
-  { name: 'Короткий лук', damage: '1d6 Колюча', specifics: 'Перезарядка - бонусна дія', properties: 'Важка зброя', characteristic: 'Спритність' }
+  { name: 'Легкий арбалет', damage: '1d8', damageType: 'Дробляча/Колюча', specifics: 'Перезарядка - основна дія', properties: 'Важка зброя', characteristic: 'Спритність' },
+  { name: 'Короткий лук', damage: '1d6', damageType: 'Колюча', specifics: 'Перезарядка - бонусна дія', properties: 'Важка зброя', characteristic: 'Спритність' }
 ]
 const WARRIOR_WEAPON_DATA: WeaponElement[] = [
-  { name: 'Бойовий молот(булава)', damage: '1d8 дробляча/колюча', specifics: 'Бонус +1 до атаки важких обладунків', properties: 'Універсальна зброя (1d10)', characteristic: 'Сила' },
-  { name: 'Бойова сокира', damage: '1d8 рубляча', specifics: 'Бонус +1 до атаки легких та середніх обладунків', properties: 'Універсальна зброя (1d10)', characteristic: 'Сила/Спритність' },
-  { name: 'Довгий меч', damage: '1d8 рубляча', specifics: 'Бонус +2 до атаки в бою з 1 супротивником', properties: 'Універсальна зброя (1d10)', characteristic: 'Сила/Спритність' },
-  { name: 'Дворучний молот(булава)', damage: '2d6 дробляча', specifics: 'Бонус +2 до атаки важких обладунків', properties: 'Важка зброя', characteristic: 'Сила' },
-  { name: 'Дворучна сокира', damage: '2d6 рубляча', specifics: 'Бонус +2 до атаки легких та середніх обладунків', properties: 'Важка зброя', characteristic: 'Сила' },
-  { name: 'Дворучний меч', damage: '2d6 рубляча/колюча', specifics: 'Бонус +4 до атаки в бою з 1 супротивником', properties: 'Важка зброя', characteristic: 'Сила' },
-  { name: 'Алебарда/Глефа', damage: '1d10 рубляча', specifics: 'Бонус +2 шкоди сусіднім ворогам', properties: 'Важка зброя', characteristic: 'Сила' }
+  { name: 'Бойовий молот(булава)', damage: '1d8', damageType: 'Дробляча/колюча', specifics: 'Бонус +1 до атаки важких обладунків', properties: 'Універсальна зброя (1d10)', characteristic: 'Сила' },
+  { name: 'Бойова сокира', damage: '1d8', damageType: 'Рубляча', specifics: 'Бонус +1 до атаки легких та середніх обладунків', properties: 'Універсальна зброя (1d10)', characteristic: 'Сила/Спритність' },
+  { name: 'Довгий меч', damage: '1d8', damageType: 'Рубляча', specifics: 'Бонус +2 до атаки в бою з 1 супротивником', properties: 'Універсальна зброя (1d10)', characteristic: 'Сила/Спритність' },
+  { name: 'Дворучний молот(булава)', damage: '2d6', damageType: 'Дробляча', specifics: 'Бонус +2 до атаки важких обладунків', properties: 'Важка зброя', characteristic: 'Сила' },
+  { name: 'Дворучна сокира', damage: '2d6', damageType: 'Рубляча', specifics: 'Бонус +2 до атаки легких та середніх обладунків', properties: 'Важка зброя', characteristic: 'Сила' },
+  { name: 'Дворучний меч', damage: '2d6', damageType: 'Рубляча/колюча', specifics: 'Бонус +4 до атаки в бою з 1 супротивником', properties: 'Важка зброя', characteristic: 'Сила' },
+  { name: 'Алебарда/Глефа', damage: '1d10', damageType: 'Рубляча', specifics: 'Бонус +2 шкоди сусіднім ворогам', properties: 'Важка зброя', characteristic: 'Сила' }
 ];
 const WARRIOR_RANGE_WEAPON_DATA: WeaponElement[] = [
-  { name: 'Одноручний арбалет', damage: '1d6 Дробляча/Колюча', specifics: 'Займає 1 руку, перезарядка 1 основна дія(Якщо в обох руках - 1 хід)', properties: 'Легка зброя', characteristic: 'Спритність' },
-  { name: 'Важкий арбалет', damage: '1d12 Дробляча/Колюча', specifics: 'Перезарядка 1 основна дія, +2 до атаки з вигідної позиції', properties: 'Важка зброя', characteristic: 'Спритність' },
-  { name: 'Довгий лук', damage: '1d8 Колюча', specifics: 'Перезарядка 1 додаткова дія, +2 до атаки з вигідної позиції', properties: 'Важка зброя', characteristic: 'Спритність' }
+  { name: 'Одноручний арбалет', damage: '1d6', damageType: 'Дробляча/Колюча', specifics: 'Займає 1 руку, перезарядка 1 основна дія(Якщо в обох руках - 1 хід)', properties: 'Легка зброя', characteristic: 'Спритність' },
+  { name: 'Важкий арбалет', damage: '1d12', damageType: 'Дробляча/Колюча', specifics: 'Перезарядка 1 основна дія, +2 до атаки з вигідної позиції', properties: 'Важка зброя', characteristic: 'Спритність' },
+  { name: 'Довгий лук', damage: '1d8', damageType: 'Колюча', specifics: 'Перезарядка 1 додаткова дія, +2 до атаки з вигідної позиції', properties: 'Важка зброя', characteristic: 'Спритність' }
 ]
 const LIGHT_ARMOUR_DATA: ArmourElement[] = [
-  { name: 'Тканинні', class: '8 + модифікатор Спр.', specifics: 'Цивільний одяг', providing: '', sneak: 'Бонус на скрадання(грабунок) +2 у населених пунктах' },
-  { name: 'Магічні', class: '8 + модифікатор Спр.', specifics: 'Бонус +2 до магічної атаки', providing: 'Інт. 12', sneak: 'Бонус на скрадання(грабунок) +2 у населених пунктах' },
+  { name: 'Тканинні', type: 'Легка', class: '8 + модифікатор Спр.', specifics: 'Цивільний одяг', providing: '', sneak: 'Бонус на скрадання(грабунок) +2 у населених пунктах' },
+  { name: 'Магічні', type: 'Легка', class: '8 + модифікатор Спр.', specifics: 'Бонус +2 до магічної атаки', providing: 'Інт. 12', sneak: 'Бонус на скрадання(грабунок) +2 у населених пунктах' },
 ]
 const MEDIUM_ARMOUR_DATA: ArmourElement[] = [
-  { name: 'Шкіряний', class: '10 + модифікатор Спр.', specifics: 'Бонус +1 до атаки зі спини або зненацька', providing: 'Спр 10', sneak: '' },
-  { name: 'Лускатий', class: '12 + модифікатор Спр.', specifics: 'Бонус +1 до атаки одноручною зброєю', providing: 'Сил 12', sneak: 'Перешкода' },
-  { name: 'Напівлатний', class: '14 + модифікатор Спр.', specifics: 'Бонус +1 до атаки дворучною зброєю', providing: 'Сил 14', sneak: 'Перешкода' }
+  { name: 'Шкіряний', type: 'Легка', class: '10 + модифікатор Спр.', specifics: 'Бонус +1 до атаки зі спини або зненацька', providing: 'Спр 10', sneak: '' },
+  { name: 'Лускатий', type: 'Легка', class: '12 + модифікатор Спр.', specifics: 'Бонус +1 до атаки одноручною зброєю', providing: 'Сил 12', sneak: 'Перешкода' },
+  { name: 'Напівлатний', type: 'Важка', class: '14 + модифікатор Спр.', specifics: 'Бонус +1 до атаки дворучною зброєю', providing: 'Сил 14', sneak: 'Перешкода' }
 ]
 const HEAVY_ARMOUR_DATA: ArmourElement[] = [
-  { name: 'Кольчуга', class: '15 + модифікатор Спр.(не більше 5)', specifics: '', providing: 'Сил. 15', sneak: 'Перешкода' },
-  { name: 'Лати', class: '18+ модифікатор Спр.(не більше 4)', specifics: '', providing: 'Сил. 15', sneak: 'Перешкода' },
+  { name: 'Кольчуга', type: 'Важка', class: '15 + модифікатор Спр.(не більше 5)', specifics: '', providing: 'Сил. 15', sneak: 'Перешкода' },
+  { name: 'Лати', type: 'Важка', class: '18+ модифікатор Спр.(не більше 4)', specifics: '', providing: 'Сил. 15', sneak: 'Перешкода' },
 ]
 const SHIELDS_ARMOUR_DATA: ArmourElement[] = [
-  { name: 'Щит', class: '2', specifics: '', providing: '', sneak: '' },
-  { name: 'Баштовий щит', class: '4 + 1/2 модифікатор Стат.', specifics: 'Можливість виконати лише 1 дію за хід(переміщення, удар, зілля). -4 до Спр.', providing: 'Сил. 14', sneak: 'Неможливе у звичайних умовах' },
+  { name: 'Щит', type: 'Легка', class: '2', specifics: '', providing: '', sneak: '' },
+  { name: 'Баштовий щит', type: 'Важка', class: '4 + 1/2 модифікатор Стат.', specifics: 'Можливість виконати лише 1 дію за хід(переміщення, удар, зілля). -4 до Спр.', providing: 'Сил. 14', sneak: 'Неможливе у звичайних умовах' },
 ]
 
 
@@ -80,6 +82,11 @@ export class ArmouryComponent implements OnInit {
       cell: (element: WeaponElement) => `${element.damage}`
     },
     {
+      columnDef: 'damageType',
+      header: 'Тип шкоди',
+      cell: (element: WeaponElement) => `${element.damageType}`
+    },
+    {
       columnDef: 'specifics',
       header: 'Особливості',
       cell: (element: WeaponElement) => `${element.specifics}`
@@ -100,6 +107,11 @@ export class ArmouryComponent implements OnInit {
       columnDef: 'name',
       header: 'Назва',
       cell: (element: ArmourElement) => `${element.name}`
+    },
+    {
+      columnDef: 'type',
+      header: 'Тип броні',
+      cell: (element: ArmourElement) => `${element.type}`
     },
     {
       columnDef: 'class',
