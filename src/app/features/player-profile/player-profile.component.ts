a3c65d80fa92a69fc5a6c4a02c8a85b64cb758ae
Vitalii <laskadrin@gmail.com>
2022-06-16 14:31:30 +0300

    display name change Done

--- src/app/features/player-profile/player-profile.component.ts
+++ src/app/features/player-profile/player-profile.component.ts
@@ -4,6 +4,8 @@ import { DashboardComponent } from '../dashboard/dashboard.component';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Auth, updateEmail, updatePassword, getAuth, reauthenticateWithCredential, EmailAuthProvider } from '@angular/fire/auth';
 import { user } from 'rxfire/auth';
+import { updateProfile } from 'firebase/auth';
+import { Observable } from 'rxjs';
 
 
 @Component({
@@ -14,11 +16,16 @@ import { user } from 'rxfire/auth';
 export class PlayerProfileComponent implements OnInit {
 
   @Output() formData: EventEmitter<{
-    email: string;
     password: string;
   }> = new EventEmitter();
-
   form: FormGroup;
+
+
+  @Output() nameFormData: EventEmitter<{
+    name: string
+  }> = new EventEmitter();
+  nameForm: FormGroup;
+
 
 
   constructor(
@@ -31,15 +38,30 @@ export class PlayerProfileComponent implements OnInit {
 
 
   user = getAuth().currentUser;
+
   newUserPassword: string;
   oldUserPassword: string;
+
   displayName = getAuth().currentUser?.displayName;
   email = getAuth().currentUser?.email;
+
   passwordChangeActive: boolean = false;
+  displayNameChangeActive: boolean = false;
+
   errorCode: string = '';
+
   changeMessage: string = '';
-  successMessage: string = ' '
+  successMessage: string = ' ';
   errorOccured: boolean = false;
+
+  changeNameMessage: string = '';
+  successNameMessage: string = '';
+  errorNameOccured: boolean = false;
+
+  photoPath: string = '../../../assets/default-profile-avatar.png';
+  selectedFile: File;
+
+  downloadURL: Observable<string>;
 
 
   ngOnInit(): void {
@@ -48,6 +70,9 @@ export class PlayerProfileComponent implements OnInit {
       newPassword: ['', Validators.required],
       newPasswordConfirm: ['', Validators.required]
     });
+    this.nameForm = this.fb.group({
+      newDisplayName: ['', Validators.required]
+    })
   }
 
   get oldPassword() {
@@ -60,20 +85,27 @@ export class PlayerProfileComponent implements OnInit {
     return this.form.get('newPasswordConfirm')
   }
 
+  get newDisplayName() {
+    return this.nameForm.get('newDisplayName')
+  }
+
   onSubmit() {
     if (this.form.valid) {
       this.formData.emit(this.form.value)
       this.changePassword();
     }
+    if (this.nameForm.valid) {
+      this.nameFormData.emit(this.nameForm.value)
+      this.changeDisplayName();
+    }
 
   }
 
   changePassword() {
-
-
     console.log('Password change button clicked');
     this.errorOccured = false;
     this.changeMessage = '';
+    this.successMessage = ''
     this.newUserPassword = this.newPassword?.value;
     if (this.newPassword?.value == this.newPasswordConfirm?.value) {
       if (this.user && typeof this.email == 'string') {
@@ -109,7 +141,7 @@ export class PlayerProfileComponent implements OnInit {
           this.errorCode = e.code;
           if (this.errorCode == 'auth/wrong-password') {
             this.errorOccured = true;
-            this.changeMessage = 'Ви ввели неправильний пароль'
+            this.changeMessage = 'Ваш старий пароль введено неправильно.'
           } else {
             this.errorOccured = true;
             this.changeMessage = 'Невідома помилка. Будь ласка, повторіть спробу входу пізніше (Помилка реавторизації)'
@@ -128,6 +160,27 @@ export class PlayerProfileComponent implements OnInit {
     this.passwordChangeActive = true;
   }
 
+  changeDisplayName() {
+    console.log('Name change button clicked');
+    this.errorNameOccured = false;
+    this.changeNameMessage = '';
+    this.successNameMessage = '';
+    if (this.user) {
+      updateProfile(this.user, { displayName: this.newDisplayName?.value }).then(() => {
+        this.displayName = this.newDisplayName?.value;
+        this.successNameMessage = 'Псевдонім успішно зміненно';
+        this.displayNameChangeActive = !this.displayNameChangeActive;
+      }).catch((e) => {
+        this.errorNameOccured = true;
+        this.changeNameMessage = 'При зміні псевдоніму сталась помилка. Спробуйте, будь ласка, пізніше' + e.code;
+      })
+    }
+
+  }
+  displayNameFormButton() {
+    this.displayNameChangeActive = !this.displayNameChangeActive;
+    this.successNameMessage = '';
+  }
 
 
 }
